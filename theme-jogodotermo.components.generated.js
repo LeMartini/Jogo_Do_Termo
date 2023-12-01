ca1024eaf7b4a550e1e0deee90231e2946ada84a
LeMartini <111762149+LeMartini@users.noreply.github.com>
2023-11-30 23:53:09 -0300

    Add files via upload

---
+++ theme-jogodotermo.components.generated.js
@@ -0,0 +1,14 @@
+
+
+
+if (!document['_vaadintheme_jogodotermo_componentCss']) {
+  
+  document['_vaadintheme_jogodotermo_componentCss'] = true;
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept((module) => {
+    window.location.reload();
+  });
+}
+
---
+++ theme.js
@@ -0,0 +1,2 @@
+import {applyTheme as _applyTheme} from './theme-jogodotermo.generated.js';
+export const applyTheme = _applyTheme;
